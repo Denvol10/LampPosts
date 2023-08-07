b794eb68487f4a436e0b2e3041e49fa9ed78b6a9
Денис Щербатюк <denvol100@gmail.com>
2023-08-07 10:24:18 +0300

    FIrst setup

--- LampPosts/Infrastructure/RevitCommand.cs
+++ LampPosts/Infrastructure/RevitCommand.cs
@@ -9,9 +9,9 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.DB.Architecture;
-using RevitWPFTemplate.ViewModels;
+using LampPosts.ViewModels;
 
-namespace RevitWPFTemplate.Infrastructure
+namespace LampPosts.Infrastructure
 {
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
